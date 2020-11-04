29af57e2307f1b887a21a5cf406048b2533368d2
Biju Sasidharan <biju.sasidharan@gmail.com>
2020-11-04 15:53:53 +0530

    python package for text processing and feature engineering

---
+++ README.md
@@ -0,0 +1,6 @@
+# Preprocessing Text Python
+Package
+
+#### Course Link: https://bit.ly/intro_nlp
+
+This pyhon package is prepared by Biju.
---
+++ preprocess_dsenthusiast/utils.py
@@ -0,0 +1,256 @@
+import re
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS as stopwords
+
+from bs4 import BeautifulSoup
+import unicodedata
+from textblob import TextBlob
+
+
+def _get_wordcounts(x):
+    length = len(str(x).split())
+    return length
+
+def _get_charcounts(x):
+    s = x.split()
+    x = ''.join(s)
+    return len(x)
+
+def _get_avg_wordlength(x):
+    count = _get_charcounts(x)/_get_wordcounts(x)
+    return count
+
+def _get_stopwords_counts(x):
+    l = len([t for t in x.split() if t in stopwords])
+    return l
+
+def _get_hashtag_counts(x):
+    l = len([t for t in x.split() if t.startswith('#')])
+    return l
+
+def _get_mentions_counts(x):
+    l = len([t for t in x.split() if t.startswith('@')])
+    return l
+
+def _get_digit_counts(x):
+    return len([t for t in x.split() if t.isdigit()])
+
+def _get_uppercase_counts(x):
+    return len([t for t in x.split() if t.isupper()])
+
+def _get_cont_exp(x):
+    contractions = {
+    "ain't": "am not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "can't've": "cannot have",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "couldn't've": "could not have",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hadn't've": "had not have",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'd've": "he would have",
+    "he'll": "he will",
+    "he'll've": "he will have",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "i'd": "i would",
+    "i'd've": "i would have",
+    "i'll": "i will",
+    "i'll've": "i will have",
+    "i'm": "i am",
+    "i've": "i have",
+    "isn't": "is not",
+    "it'd": "it had",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so is",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there had",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we had",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y'alls": "you alls",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you had",
+    "you'd've": "you would have",
+    "you'll": "you you will",
+    "you'll've": "you you will have",
+    "you're": "you are",
+    "you've": "you have",
+    " u ": " you ", 
+    " ur ": " your ",
+    " n ": " and ",
+    "i'm": "I am",
+    "i'm'a": "i am about to",
+    "i'm'o": "i am going to",
+    "innit": "is it not",
+    "dis": "this",
+    "bak": "back",
+    "brng": "bring"
+    }
+
+    if type(x) is str:
+        for key in contractions:
+            value = contractions[key]
+            x = x.replace(key, value)
+        return x
+    else:
+        return x
+
+
+def _get_emails(x):
+    emails = re.findall(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+._-]+\b)', x)
+    counts = len(emails)
+
+    return counts, emails
+
+def _remove_emails(x):
+    return re.sub(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+._-]+\b)', "", x)
+
+def _get_urls(x):
+    urls =  re.findall(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', x)
+    counts = len(urls)
+
+    return counts, urls
+
+def _remove_urls(x):
+    return re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '', x)
+
+
+def _remove_rt(x):
+    return re.sub(r'\brt\b', '', x).strip()
+
+def _remove_special_chars(x):
+    x = re.sub(r'[^\w+ ]', '', x)
+    x = ' '.join(x.split())
+    return x
+
+def _remove_html_tags(x):
+    return BeautifulSoup(x, 'lxml').get_text().strip()
+
+def _remove_accented_chars(x):
+    x = unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return x
+
+def _remove_stopwords(x):
+    return ' '.join([t for t in x.split() if t not in stopwords])
+
+def _make_base(x):
+    x = str(x)
+    x_list = []
+    doc = nlp(x)
+    
+    for token in doc:
+        lemma = token.lemma_
+        if lemma == '-PRON-' or lemma == 'be':
+            lemma = token.text
+            
+        x_list.append(lemma)
+    return ' '.join(x_list) 
+
+def _remove_commmon_words(x, n=20):
+    text = x.split()
+    freq_words = pd.Series(text).value_counts()
+    f_n = freq_words[:n]
+
+    x = ' '.join([t for t in x.split() if x not in f_n])
+    return x
+
+def _remove_rare_words(x, n=20):
+    text = x.split()
+    freq_words = pd.Series(text).value_counts()
+    r_n = freq_words.tail(n)
+
+    x = ' '.join([t for t in x.split() if x not in r_n])
+    return x
+
+def _spelling_correction(x):
+    x = ''.join(TextBlob(x).correct())
+    return x
+
